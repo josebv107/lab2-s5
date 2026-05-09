973ff3cc834b06803b59bc693dbbc8c637b26bd9
josebv107 <josebv107@gmail.com>
2026-05-09 18:13:26 -0500

    Main borrado, implementación de métodos clases hijas

--- src/PatoReal.java
+++ src/PatoReal.java
@@ -1,11 +1,11 @@
 public class PatoReal implements Pato {
     @Override
     public void cuaquear() {
-
+        System.out.println("CUAC CUAC");
     }
 
     @Override
     public void volar() {
-
+        System.out.println("Estoy volando");
     }
 }
